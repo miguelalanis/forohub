62744b6eb54f42f7f9b925d4220282b3946abeb5
Miguel Alanís <miguelalanismx@gmail.com>
2024-07-11 22:24:26 -0600

    Platilla de proyecto

---
+++ src/test/java/com/alura/ForoHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
